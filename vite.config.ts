358a5ccd36135689949235d48b28c3c4e84dbaf3
Anh Nguyen Quang <nquanganh@tma.com.vn>
2024-04-07 12:01:18 +0700

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/apple-silicon/",
   server: {
     port: 2100
   }
