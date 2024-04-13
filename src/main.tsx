70dd009814d7103dcbbc8788f1ce9418ac6ea755
Anh Nguyen Quang <nguyenquanganh38@gmail.com>
2024-04-13 10:10:26 +0700

    Add SF font, finish M2 Max

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './SFProFont.css'
 import './global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
